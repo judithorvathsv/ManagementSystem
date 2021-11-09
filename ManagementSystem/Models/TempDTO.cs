1d48035d88440cddb84621a372e562d6aaf51951
Judit Horvath <judith1horvath@outlook.com>
2021-11-09 13:25:49 +0100

    Small fix

---
+++ ManagementSystem/Models/TempDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagementSystem.Models
+{
+    public class TempDTO
+    {
+        public string Name { get; set; }
+
+        public bool Status { get; set; }
+
+        public string SentData { get; set; }
+    }
+}
